343ab4a82d99ce99560fe263ce8b4fab62d72151
zeakinky <elyandrazea@gmail.com>
2025-06-02 17:40:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🥉 Welcome to Kinkyblud")
 st.write(
     "If you could see my thoughts, you would see our faces."
 )
+st.image("https://github.com/zeakinky/zephira/blob/main/CA14F3D1-EF54-406C-B211-258A1F93958B.jpeg",width=200)
